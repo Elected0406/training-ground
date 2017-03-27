81846ddc055ad53da730b1531ef98d53cd2926b9
Elected0406 <vl.com.md2@mail.ru>
2017-03-27 10:05:27 +0300

    Add project files.

---
+++ training ground/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(training_ground.Startup))]
+namespace training_ground
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
